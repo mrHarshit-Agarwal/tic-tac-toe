631a531f66fbec9bb0d41baf81d1f65474896ba3
harshit <harshitag98.ha@gmail.com>
2023-11-30 12:10:16 +0530

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Square = ({ value, onClick, isWinningSquare }) => {
+  //   console.log(props);
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`square ${isWinningSquare? 'winning':''} ${value === 'X' ? 'text-green' : 'text-orange'}`}
+      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
